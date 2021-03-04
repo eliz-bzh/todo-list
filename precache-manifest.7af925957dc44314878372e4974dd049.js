f025206865dab6ca0ab408596429bfbbe4d20765
eliz-bzh <bozhkova.elizaveta34@gmail.com>
2021-03-04 20:52:10 +0300

    Updates

---
+++ precache-manifest.7af925957dc44314878372e4974dd049.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6afd7da5847409c310cf334dd0e38523",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "e458e5d030d7b6d7949c",
+    "url": "/todo-list/static/css/main.b255e1d5.chunk.css"
+  },
+  {
+    "revision": "7ffaa7530040e085bce9",
+    "url": "/todo-list/static/js/2.87f0bb8d.chunk.js"
+  },
+  {
+    "revision": "e458e5d030d7b6d7949c",
+    "url": "/todo-list/static/js/main.83f304f4.chunk.js"
+  },
+  {
+    "revision": "ddbec64b279924242208",
+    "url": "/todo-list/static/js/runtime-main.7637f85e.js"
+  }
+]);
